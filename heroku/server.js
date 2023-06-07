6bb3fe8e4be43f7502ddf167c829821fecb541ab
kjsmania <kjsmania@gmail.com>
2023-06-07 13:09:18 +0900

    20230607 edited server.js

--- heroku/server.js
+++ heroku/server.js
@@ -5,17 +5,12 @@ const host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable.
 const port = process.env.PORT || 8080;
 
-corsProxy.createServer({
-    originWhitelist: [
-      'http://localhost:3000',
-      'http://localhost:5000',
-      'https://portfolio-ravenous.web.app',
-      'https://portfolio-ravenous.firebaseapp.com/',
-      'https://test-my-api-endpoint.web.app',
-      'https://test-my-api-endpoint.firebaseapp.com'
-    ],
+corsProxy
+  .createServer({
+    originWhitelist: ['https://weareplasma.gatsbyjs.io'],
     requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, () => {
+    removeHeaders: ['cookie', 'cookie2'],
+  })
+  .listen(port, host, () => {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
+  });
